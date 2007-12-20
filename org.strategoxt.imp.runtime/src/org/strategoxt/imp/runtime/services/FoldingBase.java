6734982794b0fb0f09ac1d697262e6a3b87bba38
Lennart Kats <lclkats@gmail.com>
2007-12-20 15:05:04 +0000

    Refactoring: FoldingHelper is now a base class FoldingBase, and includes some logic that was previously in the generated subclass.
    svn path=/imp/trunk/org.strategoxt.imp.metatooling/; revision=18887

--- org.strategoxt.imp.runtime/src/org/strategoxt/imp/runtime/services/FoldingBase.java
+++ org.strategoxt.imp.runtime/src/org/strategoxt/imp/runtime/services/FoldingBase.java
@@ -4,60 +4,66 @@ import lpg.runtime.ILexStream;
 import lpg.runtime.IPrsStream;
 import lpg.runtime.IToken;
 
-import org.eclipse.imp.services.base.FolderBase;
 import org.strategoxt.imp.runtime.parser.ast.AstNode;
 import org.strategoxt.imp.runtime.parser.tokens.SGLRParsersym;
 
-// TODO: FoldingHelper should be the superclass of the folding component 
+public abstract class FoldingBase extends org.eclipse.imp.services.base.FolderBase {
+	private IPrsStream parseStream;
 
-public class FoldingHelper {
-	private final FolderBase folder;
-	private final IPrsStream parseStream;
-	private final ILexStream lexStream;
-	
-	public FoldingHelper(FolderBase folder, IPrsStream parseStream) {
-		this.folder = folder;
-		this.parseStream = parseStream;
-		
-		lexStream = parseStream.getLexStream();
-	}
-	
 	public void makeCompleteAnnotation(AstNode node) {
 		makeCompleteAnnotation(node.getLeftIToken(), node.getRightIToken());
 	}
-	
+
 	public void makeCompleteAnnotation(IToken firstToken, IToken lastToken) {
 		final int start = firstToken.getEndOffset();
 		int end = -1;
-		
+
 		if (firstToken.getLine() != lastToken.getLine()) {
-			// Consume any layout tokens at the end of our AST node until the next EOL		
+			// Consume any layout tokens at the end of our AST node until the
+			// next EOL
 			while (parseStream.getStreamLength() >= lastToken.getTokenIndex()) {
 				IToken next = parseStream.getTokenAt(lastToken.getTokenIndex() + 1);
-				
+
 				if (next.getKind() == SGLRParsersym.TK_LAYOUT) {
 					lastToken = next;
-					if ((end = getEndOfLinePosition(next)) != -1) break;
+					if ((end = getEndOfLinePosition(next)) != -1)
+						break;
 				} else {
 					// Next AST node starts at the same line!
 					break;
 				}
 			}
+
+			if (end == -1)
+				end = lastToken.getEndOffset();
 			
-			if (end == -1) end = lastToken.getEndOffset();
-			folder.makeAnnotation(start, end - start + 1);
+			makeAnnotation(start, end - start + 1);
 		}
 	}
-	
+
 	private int getEndOfLinePosition(IToken token) {
 		int end = token.getEndOffset();
-		
+
+		ILexStream lexStream = parseStream.getLexStream();
+
 		for (int i = token.getStartOffset(); i <= end; i++) {
 			char c = lexStream.getCharValue(i);
 			if (c == '\n' || c == '\r')
 				return i;
 		}
-		
+
 		return -1;
 	}
+
+	@Override
+	public void sendVisitorToAST(java.util.HashMap newAnnotations, java.util.List annotations,
+			java.lang.Object node) {
+		
+		AstNode astNode = (AstNode) node;
+		parseStream = astNode.getLeftIToken().getPrsStream();
+		
+		sendVisitorToAST(astNode);
+	}
+	
+	protected abstract void sendVisitorToAST(AstNode node);
 }
