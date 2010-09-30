metaborg/spoofax

commit 6c6917b69bfc065fade3973d00004211d2ab40ac

Enable refreshing of projects, not just files
svn path=/spoofax-imp/trunk/org.strategoxt.imp.runtime/; revision=21250

org.strategoxt.imp.runtime/src/org/strategoxt/imp/runtime/stratego/IMPImplodeAsfixStrategy.java
@@ -3,7 +3,7 @@ package org.strategoxt.imp.runtime.stratego;
 import java.io.File;
 import java.io.FileNotFoundException;
 
-import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
 import org.spoofax.interpreter.library.IOperatorRegistry;
 import org.spoofax.interpreter.terms.IStrategoAppl;
 import org.spoofax.interpreter.terms.IStrategoTerm;
@@ -51,7 +51,7 @@ public class IMPImplodeAsfixStrategy extends implode_asfix_1_0 {
 		}
 		
 		AstNode result = imploder.implode(asfixATerm, tokenizer);
-		IFile resource;
+		IResource resource;
 		try {
 			resource = RefreshResourcePrimitive.getResource(inputFile);
 		} catch (FileNotFoundException e) {

org.strategoxt.imp.runtime/src/org/strategoxt/imp/runtime/stratego/RefreshResourcePrimitive.java
@@ -8,7 +8,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.net.URI;
 
-import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.ResourcesPlugin;
@@ -75,7 +74,7 @@ public class RefreshResourcePrimitive extends AbstractPrimitive {
 		return true;
 	}
 
-	public static IFile getResource(IContext env, String file) throws FileNotFoundException {
+	public static IResource getResource(IContext env, String file) throws FileNotFoundException {
 		IOAgent agent = SSLLibrary.instance(env).getIOAgent();
 		File file2 = new File(file);
 		if (!file2.exists() && !file2.isAbsolute())
@@ -83,14 +82,14 @@ public class RefreshResourcePrimitive extends AbstractPrimitive {
 		return getResource(file2);
 	}
 
-	public static IFile getResource(File file) throws FileNotFoundException {
+	public static IResource getResource(File file) throws FileNotFoundException {
 		if (file == null) {
 			assert false : "file should not be null";
 			return null;
 		}
 		URI uri = file.toURI();
 		IWorkspace workspace = ResourcesPlugin.getWorkspace();
-		IFile[] resources = workspace.getRoot().findFilesForLocationURI(uri);
+		IResource[] resources = workspace.getRoot().findContainersForLocationURI(uri);
 		if (resources.length == 0)
 			throw new FileNotFoundException("File not in workspace: " + file);
 		return resources[0];

commit 34dce6aecfe166479a9c199977b22dd97cf6fc43

Fixed a regression introduced in r21250, breaking all builders :o
svn path=/spoofax-imp/trunk/org.strategoxt.imp.metatooling/; revision=21262

org.strategoxt.imp.metatooling/src/org/strategoxt/imp/metatooling/stratego/IMPParseStrategoFileStrategy.java
@@ -21,7 +21,7 @@ import org.strategoxt.imp.runtime.dynamicloading.BadDescriptorException;
 import org.strategoxt.imp.runtime.parser.JSGLRI;
 import org.strategoxt.imp.runtime.parser.ast.RootAstNode;
 import org.strategoxt.imp.runtime.services.MetaFile;
-import org.strategoxt.imp.runtime.stratego.RefreshResourcePrimitive;
+import org.strategoxt.imp.runtime.stratego.EditorIOAgent;
 import org.strategoxt.lang.Context;
 import org.strategoxt.lang.StrategoException;
 import org.strategoxt.strc.parse_stratego_file_0_0;
@@ -45,7 +45,7 @@ public class IMPParseStrategoFileStrategy extends parse_stratego_file_0_0 {
 			try {
 				stream = context.getIOAgent().openInputStream(file);
 				RootAstNode ast = parser.parse(stream, file);
-				ast.setResource(RefreshResourcePrimitive.getResource(new File(file)));
+				ast.setResource(EditorIOAgent.getResource(new File(file)));
 				return ast.getTerm();
 			} finally {
 				if (stream != null) stream.close();

org.strategoxt.imp.runtime/src/org/strategoxt/imp/runtime/services/OnSaveService.java
@@ -21,6 +21,7 @@ import org.spoofax.interpreter.terms.IStrategoTerm;
 import org.strategoxt.imp.runtime.EditorState;
 import org.strategoxt.imp.runtime.Environment;
 import org.strategoxt.imp.runtime.parser.ast.AstNode;
+import org.strategoxt.imp.runtime.stratego.EditorIOAgent;
 import org.strategoxt.imp.runtime.stratego.RefreshResourcePrimitive;
 
 /**
@@ -72,7 +73,7 @@ public class OnSaveService implements IDocumentListener, ILanguageService {
 					String file = asJavaString(termAt(result, 0));
 					String contents = asJavaString(termAt(result, 1));
 					try {
-						IFile resource = RefreshResourcePrimitive.getFile(runtime.getRuntime().getContext(), file);
+						IFile resource = EditorIOAgent.getFile(runtime.getRuntime().getContext(), file);
 						StrategoBuilder.setFileContentsDirect(resource, contents);
 					} catch (FileNotFoundException e) {
 						Environment.logException("Problem when handling on save event", e);

org.strategoxt.imp.runtime/src/org/strategoxt/imp/runtime/services/StrategoBuilder.java
@@ -42,7 +42,7 @@ import org.strategoxt.imp.runtime.Environment;
 import org.strategoxt.imp.runtime.MonitorStateWatchDog;
 import org.strategoxt.imp.runtime.RuntimeActivator;
 import org.strategoxt.imp.runtime.dynamicloading.TermReader;
-import org.strategoxt.imp.runtime.stratego.RefreshResourcePrimitive;
+import org.strategoxt.imp.runtime.stratego.EditorIOAgent;
 import org.strategoxt.imp.runtime.stratego.StrategoConsole;
 import org.strategoxt.imp.runtime.stratego.adapter.IStrategoAstNode;
 import org.strategoxt.lang.Context;
@@ -252,7 +252,7 @@ public class StrategoBuilder implements IBuilder {
 
 	private IFile getFile(IStrategoTerm resultTerm) throws FileNotFoundException {
 		String filename = asJavaString(termAt(resultTerm, 0));
-		IFile result = RefreshResourcePrimitive.getFile(
+		IFile result = EditorIOAgent.getFile(
 				observer.getRuntime().getContext(), filename);
 		return result;
 	}

org.strategoxt.imp.runtime/src/org/strategoxt/imp/runtime/stratego/EditorIOAgent.java
@@ -11,9 +11,15 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.ResourcesPlugin;
+import org.spoofax.interpreter.core.IContext;
+import org.spoofax.interpreter.library.IOAgent;
 import org.spoofax.interpreter.library.LoggingIOAgent;
+import org.spoofax.interpreter.library.ssl.SSLLibrary;
 import org.strategoxt.imp.runtime.Debug;
 import org.strategoxt.imp.runtime.dynamicloading.Descriptor;
 import org.strategoxt.imp.runtime.services.StrategoAnalysisJob;
@@ -143,5 +149,37 @@ public class EditorIOAgent extends LoggingIOAgent {
 		
 		return project;
 		
+	}
+
+	public static IFile getFile(IContext env, String file) throws FileNotFoundException {
+		IResource res = getResource(env, file);
+		if (res.getType() == IResource.FILE) {
+			return (IFile)res;
+		}
+		throw new FileNotFoundException("Resource is not a file: " + file);
+	}
+	
+	public static IResource getResource(IContext env, String file) throws FileNotFoundException {
+		IOAgent agent = SSLLibrary.instance(env).getIOAgent();
+		File file2 = new File(file);
+		if (!file2.exists() && !file2.isAbsolute())
+			file2 = new File(agent.getWorkingDir() + "/" + file);
+		return getResource(file2);
+	}
+
+	public static IResource getResource(File file) throws FileNotFoundException {
+		if (file == null) {
+			assert false : "file should not be null";
+			return null;
+		}
+		URI uri = file.toURI();
+		IWorkspace workspace = ResourcesPlugin.getWorkspace();
+		IResource[] resources = workspace.getRoot().findContainersForLocationURI(uri);
+		if (resources.length > 0 && !resources[0].exists()) // prefer files over dirs
+			resources = workspace.getRoot().findFilesForLocationURI(uri); 
+		if (resources.length == 0)
+			throw new FileNotFoundException("File not in workspace: " + file);
+		assert resources.length == 1;
+		return resources[0];
 	}
 }

org.strategoxt.imp.runtime/src/org/strategoxt/imp/runtime/stratego/IMPImplodeAsfixStrategy.java
@@ -53,7 +53,7 @@ public class IMPImplodeAsfixStrategy extends implode_asfix_1_0 {
 		AstNode result = imploder.implode(asfixATerm, tokenizer);
 		IResource resource;
 		try {
-			resource = RefreshResourcePrimitive.getResource(inputFile);
+			resource = EditorIOAgent.getResource(inputFile);
 		} catch (FileNotFoundException e) {
 			resource = null;
 		}

org.strategoxt.imp.runtime/src/org/strategoxt/imp/runtime/stratego/QueueStrategyPrimitive.java
@@ -3,11 +3,7 @@
  */
 package org.strategoxt.imp.runtime.stratego;
 
-import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.IPath;
-import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.jobs.Job;
 import org.spoofax.interpreter.core.IContext;
 import org.spoofax.interpreter.core.InterpreterException;

org.strategoxt.imp.runtime/src/org/strategoxt/imp/runtime/stratego/RefreshResourcePrimitive.java
@@ -4,14 +4,9 @@ import static org.eclipse.core.resources.IResource.DEPTH_INFINITE;
 import static org.spoofax.interpreter.core.Tools.asJavaString;
 import static org.spoofax.interpreter.core.Tools.isTermString;
 
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.net.URI;
 
-import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
-import org.eclipse.core.resources.IWorkspace;
-import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
@@ -21,8 +16,6 @@ import org.eclipse.core.runtime.jobs.Job;
 import org.spoofax.interpreter.core.IContext;
 import org.spoofax.interpreter.core.InterpreterException;
 import org.spoofax.interpreter.library.AbstractPrimitive;
-import org.spoofax.interpreter.library.IOAgent;
-import org.spoofax.interpreter.library.ssl.SSLLibrary;
 import org.spoofax.interpreter.stratego.Strategy;
 import org.spoofax.interpreter.terms.IStrategoTerm;
 
@@ -48,7 +41,7 @@ public class RefreshResourcePrimitive extends AbstractPrimitive {
 	public static boolean call(IContext env, String file) {
 		final IResource resource;
 		try {
-			resource = getResource(env, file);
+			resource = EditorIOAgent.getResource(env, file);
 		} catch (FileNotFoundException e) {
 			return false;
 		}
@@ -73,37 +66,6 @@ public class RefreshResourcePrimitive extends AbstractPrimitive {
 		job.setSystem(true);
 		job.schedule(0);
 		return true;
-	}
-
-	public static IFile getFile(IContext env, String file) throws FileNotFoundException {
-		
-		IResource res = getResource(env, file);
-		if (res.getType() == IResource.FILE) {
-			return (IFile)res;
-		}
-		throw new FileNotFoundException("Resource is not a file: " + file);
-		
-	}
-	
-	public static IResource getResource(IContext env, String file) throws FileNotFoundException {
-		IOAgent agent = SSLLibrary.instance(env).getIOAgent();
-		File file2 = new File(file);
-		if (!file2.exists() && !file2.isAbsolute())
-			file2 = new File(agent.getWorkingDir() + "/" + file);
-		return getResource(file2);
-	}
-
-	public static IResource getResource(File file) throws FileNotFoundException {
-		if (file == null) {
-			assert false : "file should not be null";
-			return null;
-		}
-		URI uri = file.toURI();
-		IWorkspace workspace = ResourcesPlugin.getWorkspace();
-		IResource[] resources = workspace.getRoot().findContainersForLocationURI(uri);
-		if (resources.length == 0)
-			throw new FileNotFoundException("File not in workspace: " + file);
-		return resources[0];
 	}
 
 }