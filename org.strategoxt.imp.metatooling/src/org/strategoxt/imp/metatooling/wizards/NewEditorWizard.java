7621820b7c689dfcb1e0c7e4553bee080fd4c9e5
Lennart Kats <lclkats@gmail.com>
2010-10-29 11:46:08 +0000

    Spoofax/282: hardcoded the project name into the launch configuration file for now.
    svn path=/spoofax-imp/trunk/org.strategoxt.imp.generator/; revision=21308

--- org.strategoxt.imp.metatooling/src/org/strategoxt/imp/metatooling/wizards/NewEditorWizard.java
+++ org.strategoxt.imp.metatooling/src/org/strategoxt/imp/metatooling/wizards/NewEditorWizard.java
@@ -2,14 +2,9 @@ package org.strategoxt.imp.metatooling.wizards;
 
 import static org.eclipse.core.resources.IResource.DEPTH_INFINITE;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
 import java.lang.reflect.InvocationTargetException;
 
-import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspace;
@@ -165,7 +160,6 @@ public class NewEditorWizard extends Wizard implements INewWizard {
 				
 				monitor.setTaskName("Building and loading example editor");
 				project.build(IncrementalProjectBuilder.FULL_BUILD, null);
-				updateProjectBuilder(project, languageName);
 				monitor.worked(6);
 
 				// TODO: Optimize - don't reload editor (already done from Ant file)
@@ -195,30 +189,6 @@ public class NewEditorWizard extends Wizard implements INewWizard {
 		EditorState.asyncOpenEditor(display, project.getFile("/test/example." + extensions.split(",")[0]), false);
 		refreshProject(project);
 		monitor.done();
-	}
-	
-	/**
-	 * Change the project build configuration to work with the Ant builder after the project
-	 * has been initially built (see create-builder-xml.str).
-	 */
-	private static void updateProjectBuilder(IProject project, String languageName) throws IOException, CoreException {
-		IFile builderConfig = project.getFile(".externalToolBuilders/" + languageName + " build.main.xml.launch");
-		String contents = readString(builderConfig.getContents(true));
-		contents = contents.replaceAll("build_project", "project_loc");
-		builderConfig.setContents(new ByteArrayInputStream(contents.getBytes()), true, false, null);
-	}
-
-	private static String readString(InputStream stream) throws IOException {
-		char[] buffer = new char[2048];
-        StringBuilder result = new StringBuilder();
-		Reader reader = new InputStreamReader(stream);
-		try {
-	        for (int read = 0; read != -1; read = reader.read(buffer))
-	            result.append(buffer, 0, read);
-	        return result.toString();
-		} finally {
-			reader.close();
-		}
 	}
 
 	private static void refreshProject(final IProject project) {
