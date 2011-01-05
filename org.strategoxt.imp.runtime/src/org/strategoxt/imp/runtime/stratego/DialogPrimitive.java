f39cce68549d68c7d7f631671ca4bd539f5685d8
MaartjeDeJonge <maartjedejonge@gmail.com>
2011-01-05 11:51:59 +0000

    svn path=/spoofax-imp/trunk/org.strategoxt.imp.runtime/; revision=21610

--- org.strategoxt.imp.runtime/src/org/strategoxt/imp/runtime/stratego/DialogPrimitive.java
+++ org.strategoxt.imp.runtime/src/org/strategoxt/imp/runtime/stratego/DialogPrimitive.java
@@ -3,17 +3,18 @@ package org.strategoxt.imp.runtime.stratego;
 import static org.spoofax.interpreter.core.Tools.isTermList;
 import static org.spoofax.interpreter.core.Tools.isTermString;
 
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.jface.dialogs.InputDialog;
+import org.eclipse.ui.progress.UIJob;
 import org.spoofax.interpreter.core.IContext;
 import org.spoofax.interpreter.core.InterpreterException;
 import org.spoofax.interpreter.library.AbstractPrimitive;
 import org.spoofax.interpreter.stratego.Strategy;
 import org.spoofax.interpreter.terms.IStrategoString;
 import org.spoofax.interpreter.terms.IStrategoTerm;
-import org.strategoxt.imp.runtime.Environment;
-
-//example: div = ?(x,y); (prim("SSL_divi",x,y) <+ prim("SSL_divr",x,y))
-//example: opendialog = prim("SSL_EXT_opendialog",id,"bla") 
 
 /**
  * @author Maartje de Jonge
@@ -21,54 +22,40 @@ import org.strategoxt.imp.runtime.Environment;
 public class DialogPrimitive extends AbstractPrimitive {
 
 	public DialogPrimitive() {
-		super("SSL_EXT_opendialog", 0, 3); //geef strategien mee (1) en termen (3 -> title, old-name,--)
+		super("SSL_EXT_opendialog", 0, 3); 
 	}
 	
 	@Override
-	public boolean call(IContext env, Strategy[] svars, IStrategoTerm[] tvars)
+	public boolean call(final IContext env, Strategy[] svars, IStrategoTerm[] tvars)
 			throws InterpreterException {
 		
-		//test input
-		//if (!is..(svars[0])) return false;
 		if (!isTermString(tvars[0]) && !isTermList(tvars[0])) return false;
 		if (!isTermString(tvars[1])) return false;
 		if (!isTermString(tvars[2])) return false;
 		
-		//geef goede namen
-		IStrategoString title = (IStrategoString)tvars[0]; 
-		IStrategoString message = (IStrategoString)tvars[1];
-		IStrategoString input = (IStrategoString)tvars[2]; 
+		final String title = ((IStrategoString)tvars[0]).stringValue(); 
+		final String message = ((IStrategoString)tvars[1]).stringValue();
+		final String input = ((IStrategoString)tvars[2]).stringValue();
 		
-		
-		Environment.getStrategoLock().lock();
+		Job job = new UIJob("user input") {
+			@Override
+			public IStatus runInUIThread(IProgressMonitor monitor) {
+				InputDialog dialog = new InputDialog(null, title, message, input, null);
+				if (dialog.open() == InputDialog.OK) {
+					String userInput=dialog.getValue();
+					env.setCurrent(env.getFactory().makeString(userInput));
+				} 
+				return Status.OK_STATUS;
+			}
+		};
+		job.setSystem(true);
+		job.schedule();	
 		try {
-			InputDialog dialog = new InputDialog(null, title.stringValue(), message.stringValue(), input.stringValue(), null);
-			if (dialog.open() == InputDialog.OK) {
-				String userInput=dialog.getValue();		
-				env.setCurrent(env.getFactory().makeString(userInput));
-				return true;
-			} 
-		} finally {
-			Environment.getStrategoLock().unlock();
+			job.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		return false;
-		/*
-		Environment.getStrategoLock().lock();
-		try {
-			//open dialog
-			Display display = new Display();
-		    final Shell shell = new Shell(display);
-			RenameDialog dialog=new RenameDialog(shell);
-			dialog.setText(title.stringValue());
-			dialog.setMessage(message.stringValue());
-			dialog.setInput(input.stringValue());
-			String userInput=dialog.open();		
-			env.setCurrent(env.getFactory().makeString(userInput));		
-		}*/		
+		return job.getResult()==Status.OK_STATUS;
 	}
-
-	
-
-	
-
 }
