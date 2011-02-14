3d0e2c2af62d576cc0ce02551f10d0d127e4d14d
MaartjeDeJonge <maartjedejonge@gmail.com>
2011-02-14 19:11:42 +0000

    syntax refactoring
    svn path=/spoofax-imp/trunk/org.strategoxt.imp.generator/; revision=21950

--- org.strategoxt.imp.generator/src/syntax/EditorService-pretty.pp
+++ org.strategoxt.imp.generator/src/syntax/EditorService-pretty.pp
@@ -1,8 +1,8 @@
 [
    PPTable                                -- R [ H [KW["pp-table"]] KW[":"] _1],
    PrettyPrint                            -- R [ H [KW["pretty-print"]] KW[":"] _1],
-   Refactoring                            -- R [ H [ KW["refactoring"]] KW[":"] _1 H [ KW["="] _2 KW["->"] _3 _4] ],
-   Refactoring.2:iter                     -- _1,
+   Refactoring                            -- R [ H [ KW["refactoring"] _1 ]  KW[":"] _2 H [  KW["="] _3 _4] ],
+   Refactoring.1:iter-star                -- _1,
    Refactoring.4:iter-star                -- _1,
    Refactorings                           -- V is=2 [H hs=0 [KW["refactorings"] _1] A(l,l,l,l,l,l) [_2]],
    ReferenceHoverRule   -- R [ H [ KW["reference"] _1 ] KW[":"] H [ _2 _3 ] ],
