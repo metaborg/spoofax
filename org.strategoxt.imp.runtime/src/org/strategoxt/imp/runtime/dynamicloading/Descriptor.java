5e8d521adc6c15b5ffa18a5ced60ad9ffca23995
Lennart Kats <lclkats@gmail.com>
2011-02-24 09:28:14 +0000

    Reparse other SugarJ editors on changes
    svn path=/spoofax-imp/trunk/org.strategoxt.imp.runtime/; revision=22129

--- org.strategoxt.imp.runtime/src/org/strategoxt/imp/runtime/dynamicloading/Descriptor.java
+++ org.strategoxt.imp.runtime/src/org/strategoxt/imp/runtime/dynamicloading/Descriptor.java
@@ -198,23 +198,18 @@ public class Descriptor {
 	}
 	
 	protected void simpleClearCache(SGLRParseController controller) {
-		Map<Class, ILanguageService> services = cachedServices.remove(controller);
-		if (services == null) return;
-		/*
-		for (AbstractServiceFactory<?> factory : serviceFactories) {
-			ILanguageService service = services.get(factory.getServiceType());
-			if (service instanceof IDynamicLanguageService) {
-		*/
+		cachedServices.remove(controller);
+	}
+	
+	protected List<IDynamicLanguageService> getActiveServices(SGLRParseController controller) {
+		List<IDynamicLanguageService> results = new ArrayList<IDynamicLanguageService>();
 		for (IDynamicLanguageService service : activeServices.keySet()) {
-			try {
-				if (service instanceof AbstractService &&
-					controller == ((AbstractService) service).internalGetParseController()) {
-					service.reinitialize(this);
-				}
-			} catch (BadDescriptorException e) {
-				Environment.logWarning("Unable to reinitialize service", e);
+			if (service instanceof AbstractService &&
+				controller == ((AbstractService) service).internalGetParseController()) {
+				results.add(service);
 			}
 		}
+		return results;
 	}
 
 	private void addKnownService(Class type, SGLRParseController controller, ILanguageService service, boolean isCachable) {
