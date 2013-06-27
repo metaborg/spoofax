ba2fb5466e89fa621884aa883c48a95cd0fe026c
oskarvanrest <oskarvanrest@gmail.com>
2013-06-27 14:14:40 -0700

    outline and properties adapter

--- org.strategoxt.imp.runtime/src/org/strategoxt/imp/runtime/SpoofaxEditor.java
+++ org.strategoxt.imp.runtime/src/org/strategoxt/imp/runtime/SpoofaxEditor.java
@@ -1,13 +1,21 @@
 package org.strategoxt.imp.runtime;
 
 import org.eclipse.imp.editor.UniversalEditor;
+import org.eclipse.ui.views.contentoutline.IContentOutlinePage;
+import org.eclipse.ui.views.properties.IPropertySource;
 
 public class SpoofaxEditor extends UniversalEditor {
 
-	
-	public SpoofaxEditor() {
-		super();
-		
-		System.out.println("SpoofaxEditor");
+	@Override
+	public Object getAdapter(Class adapter) {
+		if (adapter.equals(IContentOutlinePage.class)) {
+			super.getAdapter(adapter); // TODO
+		}
+
+		if (adapter == IPropertySource.class) {
+			super.getAdapter(adapter); // TODO
+		}
+
+		return super.getAdapter(adapter);
 	}
 }
