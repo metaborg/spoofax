84837ece1a889bda8a2580ef135b1ba358d3c3c6
oskarvanrest <oskarvanrest@gmail.com>
2013-06-27 13:52:40 -0700

    SpoofaxEditor

---
+++ org.strategoxt.imp.runtime/src/org/strategoxt/imp/runtime/SpoofaxEditor.java
@@ -0,0 +1,13 @@
+package org.strategoxt.imp.runtime;
+
+import org.eclipse.imp.editor.UniversalEditor;
+
+public class SpoofaxEditor extends UniversalEditor {
+
+	
+	public SpoofaxEditor() {
+		super();
+		
+		System.out.println("SpoofaxEditor");
+	}
+}
