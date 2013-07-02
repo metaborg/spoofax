7322c4e488e7f0fdd4a8ee657b784da51737e27b
oskarvanrest <oskarvanrest@gmail.com>
2013-07-01 18:50:02 -0700

    backwards compatibility for outline view

--- org.strategoxt.imp.runtime/src/org/strategoxt/imp/runtime/SpoofaxEditor.java
+++ org.strategoxt.imp.runtime/src/org/strategoxt/imp/runtime/SpoofaxEditor.java
@@ -3,14 +3,31 @@ package org.strategoxt.imp.runtime;
 import org.eclipse.imp.editor.UniversalEditor;
 import org.eclipse.ui.views.contentoutline.IContentOutlinePage;
 import org.eclipse.ui.views.properties.IPropertySource;
+import org.strategoxt.imp.runtime.dynamicloading.BadDescriptorException;
+import org.strategoxt.imp.runtime.services.StrategoObserver;
 import org.strategoxt.imp.runtime.services.outline.SpoofaxOutlinePage;
 
 public class SpoofaxEditor extends UniversalEditor {
 	
+	@SuppressWarnings("hiding")
+	public static final String EDITOR_ID = "org.eclipse.imp.runtime.spoofaxEditor";
+	
 	@Override
 	public Object getAdapter(Class adapter) {
+		
+		// for backward compatibility, we only instantiate a SpoofaxOutlinePage if an outline strategy is defined.
 		if (adapter.equals(IContentOutlinePage.class)) {
-			return new SpoofaxOutlinePage(EditorState.getEditorFor(getParseController()));
+			EditorState editorState = EditorState.getEditorFor(getParseController());
+			StrategoObserver observer;
+			try {
+				observer = editorState.getDescriptor().createService(StrategoObserver.class, editorState.getParseController());
+			
+				if (observer.getRuntime().lookupUncifiedSVar(SpoofaxOutlinePage.OUTLINE_STRATEGY) != null) {
+					return new SpoofaxOutlinePage(EditorState.getEditorFor(getParseController()));
+				}
+			} catch (BadDescriptorException e) {
+				e.printStackTrace();
+			}
 		}
 
 		if (adapter == IPropertySource.class) {
