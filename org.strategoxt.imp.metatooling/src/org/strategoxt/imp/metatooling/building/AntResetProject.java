1cf2b76c31ade028d790c0a242a58795f0011da7
Gabriel Konat <gabrielkonat@gmail.com>
2013-07-17 16:56:04 -0700

    Use ITaskManager instead of TaskManager.

--- org.strategoxt.imp.metatooling/src/org/strategoxt/imp/metatooling/building/AntResetProject.java
+++ org.strategoxt.imp.metatooling/src/org/strategoxt/imp/metatooling/building/AntResetProject.java
@@ -1,6 +1,6 @@
 package org.strategoxt.imp.metatooling.building;
 
-import org.metaborg.runtime.task.TaskEngine;
+import org.metaborg.runtime.task.ITaskEngine;
 import org.metaborg.runtime.task.TaskManager;
 import org.spoofax.interpreter.library.index.IIndex;
 import org.spoofax.interpreter.library.index.IndexManager;
@@ -11,7 +11,7 @@ public class AntResetProject {
 		final String projectPath = args[0];
 
 		final TaskManager taskManager = TaskManager.getInstance();
-		final TaskEngine taskEngine = taskManager.getTaskEngine(projectPath);
+		final ITaskEngine taskEngine = taskManager.getTaskEngine(projectPath);
 		if(taskEngine != null)
 			taskEngine.reset();
 
