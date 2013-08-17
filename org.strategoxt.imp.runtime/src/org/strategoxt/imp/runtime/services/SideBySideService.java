5fe77f1d17ee01ff11b4e9729b5f37777d072b7f
Gabriel Konat <gabrielkonat@gmail.com>
2013-08-17 10:00:56 -0700

    Remove reference to legacy sidebyside project.

--- org.strategoxt.imp.runtime/src/org/strategoxt/imp/runtime/services/SideBySideService.java
+++ org.strategoxt.imp.runtime/src/org/strategoxt/imp/runtime/services/SideBySideService.java
@@ -1,9 +1,7 @@
 package org.strategoxt.imp.runtime.services;
 
 import org.stratego.imp.runtime.services.sidebyside.latest.LatestSidePaneEditorHelper;
-import org.stratego.imp.runtime.services.sidebyside.legacy.LegacySidePaneEditorHelper;
 import org.stratego.imp.runtime.services.sidebyside.main.SidePaneEditorHelper;
-import org.strategoxt.imp.runtime.Environment;
 
 
 public class SideBySideService {
@@ -11,14 +9,7 @@ public class SideBySideService {
 	private static SidePaneEditorHelper helper;
 	
 	public static SidePaneEditorHelper helperInstance() {
-		if(helper == null){
-			try{
-				helper = new LegacySidePaneEditorHelper();
-			} catch(Throwable t) {
-				Environment.logWarning("Legacy side-by-side not supported. Loading dummy.");
-				helper = new LatestSidePaneEditorHelper();
-			}
-		}
+		helper = new LatestSidePaneEditorHelper();
 		return helper;
 	}
 	
