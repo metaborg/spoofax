5a9600f492a874f217f1febf8221e2b5034f3111
oskarvanrest <oskarvanrest@gmail.com>
2013-09-25 14:22:46 -0700

    bugfix: after closing and reopening the outline popup, the filter is now reset to ""

--- org.strategoxt.imp.runtime/src/org/strategoxt/imp/runtime/services/views/outline/SpoofaxOutlinePopup.java
+++ org.strategoxt.imp.runtime/src/org/strategoxt/imp/runtime/services/views/outline/SpoofaxOutlinePopup.java
@@ -49,6 +49,7 @@ public class SpoofaxOutlinePopup extends FilteringInfoPopup {
 	protected void handleElementSelected(Object selectedElement) {
 		if (selectedElement != null) {
 			SpoofaxOutlineUtil.selectCorrespondingText(selectedElement, parseController);
+			setMatcherString("", true);
 		}
 	}
 
