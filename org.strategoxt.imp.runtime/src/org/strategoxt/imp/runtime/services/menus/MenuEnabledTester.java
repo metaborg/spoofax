a6aea2a72596a658cfbd681e65f7c4968acf68ab
oskarvanrest <oskarvanrest@gmail.com>
2013-10-18 10:26:34 -0700

    Menu refreshing: now for multiple menus

--- org.strategoxt.imp.runtime/src/org/strategoxt/imp/runtime/services/menus/MenuEnabledTester.java
+++ org.strategoxt.imp.runtime/src/org/strategoxt/imp/runtime/services/menus/MenuEnabledTester.java
@@ -9,28 +9,37 @@ public class MenuEnabledTester extends PropertyTester {
 
 	private IWorkbenchPart activePart;
 	private double time;
+	private boolean result;
+	private int menuIndex = -1;
 
 	@Override
 	public synchronized boolean test(Object receiver, String property, Object[] args, Object expectedValue) {
-
-		boolean result = false;
 		
 		if (property.equals("menuEnabled")) {
 
+			menuIndex = (menuIndex + 1) % MenusServiceConstants.NO_OF_TOOLBAR_MENUS;
+			if (menuIndex == 0) {
+				result = false;
+			}
+			else {
+				return result;
+			}
+			
 			IWorkbenchPart lastActivePart = activePart;
 			activePart = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActivePart();
 			
 			double newTime = System.currentTimeMillis();
-			if (newTime - time > 100) {
+			if (newTime - time > 50) { // hack: hide and show menu to trigger refresh of size and label 
 				time = newTime;
 				if (lastActivePart instanceof SpoofaxEditor) {
 					return false; // refresh
 				}
 			}
 			
-			return activePart instanceof SpoofaxEditor;			
+			result = activePart instanceof SpoofaxEditor;
+			return result;
 		}
 		
-		return result;
+		return false;
 	}
 }
