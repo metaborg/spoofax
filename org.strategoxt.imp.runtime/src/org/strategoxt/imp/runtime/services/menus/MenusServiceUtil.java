dad5cb552bbe4e885d055495393ea4cf59ee7340
oskarvanrest <oskarvanrest@gmail.com>
2013-10-18 13:10:01 -0700

    RefreshToolbarMenus()

---
+++ org.strategoxt.imp.runtime/src/org/strategoxt/imp/runtime/services/menus/MenusServiceUtil.java
@@ -0,0 +1,32 @@
+package org.strategoxt.imp.runtime.services.menus;
+
+import org.eclipse.jface.action.ICoolBarManager;
+import org.eclipse.jface.action.ToolBarContributionItem;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.commands.ICommandService;
+import org.eclipse.ui.internal.WorkbenchWindow;
+
+@SuppressWarnings("restriction")
+public class MenusServiceUtil {
+
+	public static void refreshToolbarMenus() {
+		IWorkbench wb = PlatformUI.getWorkbench();
+		IWorkbenchWindow window = wb.getActiveWorkbenchWindow();
+
+		if (window instanceof WorkbenchWindow && ((WorkbenchWindow) window).getCoolBarVisible()) {
+			ICoolBarManager coolBarManager = ((WorkbenchWindow) window).getCoolBarManager2();
+			ToolBarContributionItem menu = (ToolBarContributionItem) coolBarManager.find(MenusServiceConstants.TOOLBAR_ID);
+			
+			ICommandService commandService = (ICommandService) wb.getService(ICommandService.class);
+			for (int i = 1; i <= MenusServiceConstants.NO_OF_TOOLBAR_MENUS; i++) {
+				commandService.refreshElements(MenusServiceConstants.TOOLBAR_BASECOMMAND_ID_PREFIX + i, null);
+			}
+			
+			menu.getToolBarManager().update(true);
+			menu.setVisible(false);
+			menu.setVisible(true);
+		}
+	}
+}
