bd60e4058ab621a7fc2647c50eef52696e17bb25
Oskar van Rest <oskarvanrest@gmail.com>
2013-10-21 15:38:29 -0700

    Set proper menu captions. Excecute last executed action is one exists, and the first defined one otherwise.

--- org.strategoxt.imp.runtime/src/org/strategoxt/imp/runtime/services/menus/ToolbarBaseCommandHandler.java
+++ org.strategoxt.imp.runtime/src/org/strategoxt/imp/runtime/services/menus/ToolbarBaseCommandHandler.java
@@ -1,5 +1,7 @@
 package org.strategoxt.imp.runtime.services.menus;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
@@ -10,12 +12,22 @@ import org.eclipse.core.commands.IHandlerListener;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.commands.IElementUpdater;
 import org.eclipse.ui.menus.UIElement;
+import org.strategoxt.imp.runtime.EditorState;
+import org.strategoxt.imp.runtime.services.menus.contribs.IBuilder;
+import org.strategoxt.imp.runtime.services.menus.contribs.IMenuContribution;
+import org.strategoxt.imp.runtime.services.menus.contribs.Menu;
 
 /**
  * @author Oskar van Rest
  */
 public class ToolbarBaseCommandHandler implements IHandler, IElementUpdater {
-	
+
+	private ArrayList<List<String>> lastActions = new ArrayList<>(MenusServiceConstants.NO_OF_TOOLBAR_MENUS);
+
+	public void setLastAction(int menuIndex, List<String> pathOfLastAction) {
+		lastActions.set(menuIndex, pathOfLastAction);
+	}
+
 	@Override
 	public void addHandlerListener(IHandlerListener handlerListener) {
 	}
@@ -26,11 +38,48 @@ public class ToolbarBaseCommandHandler implements IHandler, IElementUpdater {
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		String menuID = event.getParameter(MenusServiceConstants.MENU_ID_PARAM);
-		System.out.println(menuID);
+		int menuIndex = Integer.parseInt((String) event.getParameter(MenusServiceConstants.MENU_ID_PARAM));
+		MenuList menus = MenusServiceUtil.getMenus();
+
+		IBuilder builder = null;
+
+		List<String> lastAction = lastActions.get(menuIndex);
+		if (lastAction != null) {
+			builder = menus.getBuilder(lastAction);
+		}
+
+		if (builder == null) {
+			builder = getSomeAction(menus.getAll().get(menuIndex));
+		}
+
+		if (builder != null) {
+			builder.scheduleExecute(EditorState.getActiveEditor(), null, null, false);
+		}
+
 		return null;
 	}
-	
+
+	private IBuilder getSomeAction(Menu menu) {
+		for (IMenuContribution contrib : menu.getMenuContributions()) {
+			switch (contrib.getContributionType()) {
+			case IMenuContribution.BUILDER:
+				return (IBuilder) contrib;
+
+			case IMenuContribution.MENU:
+				IBuilder action = getSomeAction((Menu) contrib);
+				if (action != null) {
+					return action;
+				} else {
+					break;
+				}
+			default:
+				break;
+			}
+		}
+
+		return null;
+	}
+
 	@Override
 	public boolean isEnabled() {
 		return true;
@@ -44,40 +93,24 @@ public class ToolbarBaseCommandHandler implements IHandler, IElementUpdater {
 	@Override
 	public void removeHandlerListener(IHandlerListener handlerListener) {
 	}
-	
-	
 
 	@Override
 	public void updateElement(UIElement element, @SuppressWarnings("rawtypes") Map parameters) {
 		int menuIndex = Integer.parseInt((String) parameters.get(MenusServiceConstants.MENU_ID_PARAM));
-		int a = new Random().nextInt(10);
-		int b = new Random().nextInt(10);
-		int c = new Random().nextInt(10);
-		String a2 = a > 5? "": "" + a;
-		String b2 = a > 5? "": "" + b;
-		String c2 = a > 5? "": "" + c;
+		MenuList menus = MenusServiceUtil.getMenus();
 		
-		element.setText("menu " + a2 + b2 + c2);
-		element.setTooltip("");
-				
-//		EditorState editor = EditorState.getActiveEditor();
-//
-//		if (editor != null) {
-//			MenusService menusService = null;
-//			try {
-//				menusService = editor.getDescriptor().createService(MenusService.class, editor.getParseController());
-//				enabled = true; // TODO
-//			} catch (BadDescriptorException e) {
-//				e.printStackTrace();
-//			}
-//		}
-//		if (menuIndex == MenusServiceConstants.NO_OF_TOOLBAR_MENUS-1) {
+		if (menus.getAll().size() > menuIndex) {
+			String caption = menus.getAll().get(menuIndex).getCaption();
+			
+			element.setText(caption);
+			element.setTooltip("");
+
 			Display.getDefault().asyncExec(new Runnable() {
 				@Override
 				public void run() {
 					MenusServiceUtil.refreshToolbarMenus();
 				}
 			});
-//		}
+		}
 	}
 }
