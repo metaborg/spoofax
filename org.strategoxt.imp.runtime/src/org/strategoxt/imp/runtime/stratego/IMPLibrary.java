2b71ad3cc037487b5e69971bc7a5f3f3f705eb24
Gabriel Konat <gabrielkonat@gmail.com>
2014-05-16 11:34:27 +0200

    Add primitive that preserves annotations and attachments of the current term, after applying a strategy.

--- org.strategoxt.imp.runtime/src/org/strategoxt/imp/runtime/stratego/IMPLibrary.java
+++ org.strategoxt.imp.runtime/src/org/strategoxt/imp/runtime/stratego/IMPLibrary.java
@@ -6,9 +6,9 @@ import org.spoofax.interpreter.library.AbstractStrategoOperatorRegistry;
  * @author Lennart Kats <lennart add lclnet.nl>
  */
 public class IMPLibrary extends AbstractStrategoOperatorRegistry {
-	
+
 	public static final String REGISTRY_NAME = "sdf2imp";
-	
+
 	public IMPLibrary() {
 		add(new NameDialogPrimitive());
 		add(new SubtermPrimitive());
@@ -24,7 +24,7 @@ public class IMPLibrary extends AbstractStrategoOperatorRegistry {
 		add(new CandidateSortsPrimitive());
 		add(new GetAllProjectsPrimitive());
 		add(new ForeignLangCallPrimitive());
-		
+
 		add(new SetTotalWorkUnitsPrimitive());
 		add(new CompleteWorkUnitPrimitive());
 		add(new SaveAllResourcesPrimitive());
@@ -37,6 +37,8 @@ public class IMPLibrary extends AbstractStrategoOperatorRegistry {
 		add(new InSelectedFragmentPrimitive());
 
 		add(new OriginLanguagePrimitive());
+
+		add(new PreserveAnnotationsAttachmentsPrimitive());
 	}
 
 	public String getOperatorRegistryName() {
