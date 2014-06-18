57855252b8c15a57667e756e5653cbf871f5f575
Vlad Vergu <v.vergu@gmail.com>
2014-06-18 17:34:52 +0200

    Cleanup build file. Add script to download the strategoxt distribution

--- org.strategoxt.imp.generator/build.sh
+++ org.strategoxt.imp.generator/build.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-ANT_OPTS="-Xss32m -Xmx1024m -server -XX:+UseParallelGC -XX:MaxPermSize=256m" ant -lib strategoxt.jar $@
+ANT_OPTS="-Xss32m -Xmx1024m -server -XX:+UseParallelGC -XX:MaxPermSize=256m" ant -lib strategoxt-distrib/share/strategoxt/strategoxt/strategoxt.jar $@
 
