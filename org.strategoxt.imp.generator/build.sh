292deae6058a398c314ac9bb8049a6fb3c5bc497
Vlad Vergu <v.vergu@gmail.com>
2014-06-18 16:27:04 +0200

    No longer assume that we should run install Ant task

--- org.strategoxt.imp.generator/build.sh
+++ org.strategoxt.imp.generator/build.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-ANT_OPTS="-Xss32m -Xmx1024m -server -XX:+UseParallelGC -XX:MaxPermSize=256m" ant -lib strategoxt.jar $@ install
+ANT_OPTS="-Xss32m -Xmx1024m -server -XX:+UseParallelGC -XX:MaxPermSize=256m" ant -lib strategoxt.jar $@
 
