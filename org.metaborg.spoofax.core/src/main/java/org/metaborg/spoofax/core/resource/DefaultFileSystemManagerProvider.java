9ed8051954e54c5e1ec7bb13d49f2283b233dbe5
Gabriel Konat <gabrielkonat@gmail.com>
2014-11-05 13:01:18 +0100

    Add common VFS file providers.

--- org.metaborg.spoofax.core/src/main/java/org/metaborg/spoofax/core/resource/DefaultFileSystemManagerProvider.java
+++ org.metaborg.spoofax.core/src/main/java/org/metaborg/spoofax/core/resource/DefaultFileSystemManagerProvider.java
@@ -5,15 +5,27 @@ import org.apache.commons.vfs2.FileSystemException;
 import org.apache.commons.vfs2.FileSystemManager;
 import org.apache.commons.vfs2.cache.DefaultFilesCache;
 import org.apache.commons.vfs2.impl.DefaultFileSystemManager;
+import org.apache.commons.vfs2.provider.bzip2.Bzip2FileProvider;
+import org.apache.commons.vfs2.provider.ftp.FtpFileProvider;
+import org.apache.commons.vfs2.provider.ftps.FtpsFileProvider;
+import org.apache.commons.vfs2.provider.gzip.GzipFileProvider;
+import org.apache.commons.vfs2.provider.https.HttpsFileProvider;
+import org.apache.commons.vfs2.provider.jar.JarFileProvider;
 import org.apache.commons.vfs2.provider.local.DefaultLocalFileProvider;
 import org.apache.commons.vfs2.provider.ram.RamFileProvider;
 import org.apache.commons.vfs2.provider.res.ResourceFileProvider;
+import org.apache.commons.vfs2.provider.tar.TarFileProvider;
+import org.apache.commons.vfs2.provider.tar.Tbz2FileProvider;
+import org.apache.commons.vfs2.provider.tar.TgzFileProvider;
+import org.apache.commons.vfs2.provider.temp.TemporaryFileProvider;
+import org.apache.commons.vfs2.provider.webdav.WebdavFileProvider;
+import org.apache.commons.vfs2.provider.zip.ZipFileProvider;
 
 import com.google.inject.Provider;
 
 /**
- * Guice provider for the VFS file system manager. Subclass and override the protected methods, and bind the provider in
- * your Guice module, to customize the file providers.
+ * Guice provider for the VFS file system manager. Subclass and override the protected methods, and bind the
+ * provider in your Guice module, to customize the file providers.
  */
 public class DefaultFileSystemManagerProvider implements Provider<FileSystemManager> {
     @Override public FileSystemManager get() {
@@ -39,7 +51,20 @@ public class DefaultFileSystemManagerProvider implements Provider<FileSystemMana
     }
 
     protected void addProviders(DefaultFileSystemManager manager) throws FileSystemException {
+        manager.addProvider("tmp", new TemporaryFileProvider());
         manager.addProvider("res", new ResourceFileProvider());
         manager.addProvider("ram", new RamFileProvider());
+        manager.addProvider("zip", new ZipFileProvider());
+        manager.addProvider("jar", new JarFileProvider());
+        manager.addProvider("tar", new TarFileProvider());
+        manager.addProvider("tgz", new TgzFileProvider());
+        manager.addProvider("tbz2", new Tbz2FileProvider());
+        manager.addProvider("gz", new GzipFileProvider());
+        manager.addProvider("bz2", new Bzip2FileProvider());
+        manager.addProvider("http", new HttpsFileProvider());
+        manager.addProvider("https", new HttpsFileProvider());
+        manager.addProvider("webdav", new WebdavFileProvider());
+        manager.addProvider("ftp", new FtpFileProvider());
+        manager.addProvider("ftps", new FtpsFileProvider());
     }
 }
