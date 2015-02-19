f35d2b90c72813f717f6b56de4af6c52e318c609
Sebastian Erdweg <seba.github@gmail.com>
2015-02-19 15:59:41 +0100

    fix array concat

--- org.metaborg.spoofax.build.cleardep/src/org/metaborg/spoofax/build/cleardep/util/Util.java
+++ org.metaborg.spoofax.build.cleardep/src/org/metaborg/spoofax/build/cleardep/util/Util.java
@@ -1,9 +1,11 @@
 package org.metaborg.spoofax.build.cleardep.util;
 
+import java.lang.reflect.Array;
+
 public class Util{
 	public static <T> T[] arrayConcat(T[] ar1, T[] ar2) {
 		@SuppressWarnings("unchecked")
-		T[] ar = (T[]) new Object[ar1.length + ar2.length];
+		T[] ar = (T[]) Array.newInstance(ar1.getClass().getComponentType(), ar1.length + ar2.length);
 		
 		System.arraycopy(ar1, 0, ar, 0, ar1.length);
 		System.arraycopy(ar2, 0, ar, ar1.length, ar2.length);
