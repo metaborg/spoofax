2d158f07c912adb1a20829fa48f466a5e46801ae
Sebastian Erdweg <seba.github@gmail.com>
2015-02-19 09:25:50 +0100

    track required files of Stratego compiler call

--- org.metaborg.spoofax.build.cleardep/src/org/metaborg/spoofax/build/cleardep/builders/StrategoJavaCompiler.java
+++ org.metaborg.spoofax.build.cleardep/src/org/metaborg/spoofax/build/cleardep/builders/StrategoJavaCompiler.java
@@ -2,18 +2,21 @@ package org.metaborg.spoofax.build.cleardep.builders;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.regex.Pattern;
 
 import org.metaborg.spoofax.build.cleardep.LoggingFilteringIOAgent;
 import org.metaborg.spoofax.build.cleardep.SpoofaxBuildContext;
 import org.metaborg.spoofax.build.cleardep.StrategoExecutor;
 import org.metaborg.spoofax.build.cleardep.StrategoExecutor.ExecutionResult;
 import org.sugarj.cleardep.CompilationUnit.State;
+import org.sugarj.cleardep.CompilationUnit;
 import org.sugarj.cleardep.SimpleCompilationUnit;
 import org.sugarj.cleardep.build.Builder;
 import org.sugarj.cleardep.build.BuilderFactory;
 import org.sugarj.cleardep.stamp.LastModifiedStamper;
 import org.sugarj.cleardep.stamp.Stamper;
 import org.sugarj.common.FileCommands;
+import org.sugarj.common.path.AbsolutePath;
 import org.sugarj.common.path.Path;
 import org.sugarj.common.path.RelativePath;
 
@@ -67,8 +70,17 @@ public class StrategoJavaCompiler extends Builder<SpoofaxBuildContext, StrategoJ
 	public void build(SimpleCompilationUnit result, Input input) throws IOException {
 		result.addSourceArtifact(input.inputPath);
 		
+		Path rtree = FileCommands.replaceExtension(input.outputPath, "rtree");
+		Path strdep = FileCommands.addExtension(input.outputPath, "dep");
+
+		FileCommands.delete(rtree);
+		
 		ExecutionResult er = StrategoExecutor.runStrategoCLI(context.strjContext(), 
-				org.strategoxt.strj.main_0_0.instance, "strj", new LoggingFilteringIOAgent(),
+				org.strategoxt.strj.main_0_0.instance, "strj", 
+				new LoggingFilteringIOAgent(
+						Pattern.quote("[ strj | info ]") + ".*", 
+                        Pattern.quote("[ strj | error ] Compilation failed") + ".*",
+                        Pattern.quote("[ strj | warning ] Nullary constructor") + ".*"),
 				"-i", input.inputPath,
 				"-o", input.outputPath,
 				"-p", "trans",
@@ -77,16 +89,30 @@ public class StrategoJavaCompiler extends Builder<SpoofaxBuildContext, StrategoJ
 				input.buildStrategoArgs,
 				input.externaljarflags,
 				input.externalDef != null ? "-I " + input.externalDef : "",
+				"-I", context.baseDir,
 				"-I", context.basePath("${lib}"),
 				"-I", context.basePath("${include}"),
-				"-I", context.baseDir,
 				"-I", context.basePath("${trans}"),
 				"--cache-dir", context.basePath(".cache"));
+		FileCommands.delete(rtree);
 		
 		result.addGeneratedFile(input.outputPath);
-		Path strdep = FileCommands.replaceExtension(input.outputPath, "dep");
-		// TODO extract dependencies from `strdep`
+		result.addGeneratedFile(rtree);
+		result.addGeneratedFile(strdep);
+		
+		registerUsedPaths(result, strdep);
 		
 		result.setState(State.finished(er.success));
+	}
+	
+	private void registerUsedPaths(CompilationUnit result, Path strdep) throws IOException {
+		String contents = FileCommands.readFileAsString(strdep);
+		String[] lines = contents.split("[\\s\\\\]+");
+		for (int i = 1; i < lines.length; i++) { // skip first line, which lists the generated ctree file
+			String line = lines[i];
+			Path p = new AbsolutePath(line);
+			RelativePath prel = FileCommands.getRelativePath(context.baseDir, p);
+			result.addExternalFileDependency(prel != null ? prel : p);
+		}
 	}
 }
