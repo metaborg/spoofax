7e3a7e6fe53c4e761e652f7b10171a1f7396ebb5
Sebastian Erdweg <seba.github@gmail.com>
2015-02-20 14:52:54 +0100

    create JavaBuilder.Input

--- org.metaborg.spoofax.build.cleardep/src/org/metaborg/spoofax/build/cleardep/builders/SpoofaxDefaultCtree.java
+++ org.metaborg.spoofax.build.cleardep/src/org/metaborg/spoofax/build/cleardep/builders/SpoofaxDefaultCtree.java
@@ -5,12 +5,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.metaborg.spoofax.build.cleardep.SpoofaxBuildContext;
+import org.metaborg.spoofax.build.cleardep.util.FileExtensionFilter;
 import org.strategoxt.imp.metatooling.building.AntForceRefreshScheduler;
 import org.sugarj.cleardep.CompilationUnit;
 import org.sugarj.cleardep.SimpleCompilationUnit;
 import org.sugarj.cleardep.SimpleMode;
+import org.sugarj.cleardep.build.BuildContext;
 import org.sugarj.cleardep.build.Builder;
 import org.sugarj.cleardep.build.BuilderFactory;
+import org.sugarj.cleardep.buildjava.JavaBuilder;
 import org.sugarj.cleardep.buildjava.JavaJar;
 import org.sugarj.cleardep.stamp.LastModifiedStamper;
 import org.sugarj.cleardep.stamp.Stamper;
@@ -114,23 +117,50 @@ public class SpoofaxDefaultCtree extends Builder<SpoofaxBuildContext, Void, Simp
 		boolean debug = true;
 		String sourceVersion = "1.7";
 		String targetVersion = "1.7";
+		List<String> additionalArgs = new ArrayList<>();
+		if (debug)
+			additionalArgs.add("-g");
+		additionalArgs.add("-source");
+		additionalArgs.add(sourceVersion);
+		additionalArgs.add("-target");
+		additionalArgs.add(targetVersion);
 		
 		String srcDirs = context.props.getOrElse("src-dirs", context.props.get("src-gen"));
-		List<String> sourcePath = new ArrayList<>();
-		for (String dir : srcDirs.split("[\\s]+"))
-			sourcePath.add(dir);
+		List<Path> sourcePath = new ArrayList<>();
+		List<Path> sourceFiles = new ArrayList<>();
+		for (String dir : srcDirs.split("[\\s]+")) {
+			Path p;
+			if (AbsolutePath.acceptable(dir))
+				p = new AbsolutePath(dir);
+			else
+				p = context.basePath(dir);
+			
+			sourcePath.add(p);
+			sourceFiles.addAll(FileCommands.listFilesRecursive(p, new FileExtensionFilter("java")));
+		}
 		
-		List<String> classPath = new ArrayList<>();
-		classPath.add(context.props.getOrFail("eclipse.spoofaximp.strategominjar"));
-		classPath.add(context.basePath("${src-gen}").getAbsolutePath());
+		
+		List<Path> classPath = new ArrayList<>();
+		classPath.add(new AbsolutePath(context.props.getOrFail("eclipse.spoofaximp.strategominjar")));
+		classPath.add(context.basePath("${src-gen}"));
 		if (context.props.isDefined("externaljar"))
-			classPath.add(context.props.get("externaljar"));
+			classPath.add(new AbsolutePath(context.props.get("externaljar")));
 		if (context.props.isDefined("externaljarx"))
-			classPath.add(context.props.get("externaljarx"));
+			classPath.add(new AbsolutePath(context.props.get("externaljarx")));
 		if (context.isJavaJarEnabled(result))
-			classPath.add(context.basePath("${include}/${strmodule}-java.jar").getAbsolutePath());
+			classPath.add(context.basePath("${include}/${strmodule}-java.jar"));
 
-		// TODO call JavaBuilder.require(..)
+		
+		CompilationUnit javac = JavaBuilder.factory.makeBuilder(new BuildContext(context.getBuildManager())).require(
+				new JavaBuilder.Input(
+						sourceFiles,
+						targetDir,
+						sourcePath, 
+						classPath,
+						additionalArgs,
+						null),
+				new SimpleMode());
+		result.addModuleDependency(javac);
 	}
 
 	private void javaJar(SimpleCompilationUnit result, String strmodule) throws IOException {
