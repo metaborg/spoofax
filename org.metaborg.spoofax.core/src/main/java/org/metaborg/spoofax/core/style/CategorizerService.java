56534007c2443834298326721a22260f6b164d2a
Gabriel Konat <gabrielkonat@gmail.com>
2015-03-11 16:16:06 +0100

    Fallback to regular categorization if getting a category for the parent term fails. Add some toString's for debugging.

--- org.metaborg.spoofax.core/src/main/java/org/metaborg/spoofax/core/style/CategorizerService.java
+++ org.metaborg.spoofax.core/src/main/java/org/metaborg/spoofax/core/style/CategorizerService.java
@@ -9,12 +9,12 @@ import org.metaborg.spoofax.core.syntax.ParseResult;
 import org.metaborg.spoofax.core.syntax.jsglr.JSGLRSourceRegionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.spoofax.interpreter.terms.ISimpleTerm;
 import org.spoofax.interpreter.terms.IStrategoAppl;
 import org.spoofax.interpreter.terms.IStrategoTerm;
 import org.spoofax.jsglr.client.imploder.IToken;
 import org.spoofax.jsglr.client.imploder.ITokenizer;
 import org.spoofax.jsglr.client.imploder.ImploderAttachment;
+import org.spoofax.terms.attachments.ParentAttachment;
 
 import com.google.common.collect.Lists;
 import com.google.inject.Inject;
@@ -38,39 +38,10 @@ public class CategorizerService implements ICategorizerService<IStrategoTerm, IS
         final int tokenCount = tokenzier.getTokenCount();
         for(int i = 0; i < tokenCount; ++i) {
             final IToken token = tokenzier.getTokenAt(i);
-            final ISimpleTerm term = token.getAstNode();
-
-            final ICategory category;
-            if(term == null) {
-                category = tokenCategory(token);
-            } else {
-                final IStrategoTerm strategoTerm = (IStrategoTerm) term;
-                final ImploderAttachment imploderAttachment = ImploderAttachment.get(strategoTerm);
-                final String sort = imploderAttachment.getSort();
-                if(strategoTerm.getTermType() == IStrategoTerm.APPL) {
-                    final String cons = ((IStrategoAppl) strategoTerm).getConstructor().getName();
-
-                    if(facet.hasSortConsStyle(sort, cons)) {
-                        category = new SortConsCategory(sort, cons);
-                    } else if(facet.hasConsStyle(cons)) {
-                        category = new ConsCategory(cons);
-                    } else if(facet.hasSortStyle(sort)) {
-                        category = new SortCategory(sort);
-                    } else {
-                        category = tokenCategory(token);
-                    }
-                } else {
-                    if(facet.hasSortStyle(sort)) {
-                        category = new SortCategory(sort);
-                    } else {
-                        category = tokenCategory(token);
-                    }
-                }
-            }
-
+            final ICategory category = category(facet, token);
             if(category != null) {
                 final ISourceRegion region = JSGLRSourceRegionFactory.fromToken(token);
-                regionCategories.add(new RegionCategory<IStrategoTerm>(null, region, category));
+                regionCategories.add(new RegionCategory<IStrategoTerm>(region, category, null));
             }
         }
 
@@ -82,6 +53,54 @@ public class CategorizerService implements ICategorizerService<IStrategoTerm, IS
         throw new UnsupportedOperationException();
     }
 
+
+    private ICategory category(StylerFacet facet, IToken token) {
+        IStrategoTerm term = (IStrategoTerm) token.getAstNode();
+        if(term == null) {
+            return tokenCategory(token);
+        }
+
+        final int termType = term.getTermType();
+        if(termType != IStrategoTerm.APPL && termType != IStrategoTerm.TUPLE && termType != IStrategoTerm.LIST) {
+            // Try to use the parent of terminal nodes, mimicking behavior of old Spoofax/IMP runtime.
+            final IStrategoTerm parentTerm = ParentAttachment.getParent(term);
+            if(parentTerm != null) {
+                final ICategory category = sortConsCategory(facet, parentTerm);
+                if(category != null) {
+                    return category;
+                }
+            }
+        }
+
+        final ICategory category = sortConsCategory(facet, term);
+        if(category != null) {
+            return category;
+        }
+
+        return tokenCategory(token);
+    }
+
+    private ICategory sortConsCategory(StylerFacet facet, IStrategoTerm term) {
+        final ImploderAttachment imploderAttachment = ImploderAttachment.get(term);
+        final String sort = imploderAttachment.getSort();
+        if(term.getTermType() == IStrategoTerm.APPL) {
+            final String cons = ((IStrategoAppl) term).getConstructor().getName();
+            if(facet.hasSortConsStyle(sort, cons)) {
+                return new SortConsCategory(sort, cons);
+            } else if(facet.hasConsStyle(cons)) {
+                return new ConsCategory(cons);
+            } else if(facet.hasSortStyle(sort)) {
+                return new SortCategory(sort);
+            }
+            return null;
+        }
+
+        if(facet.hasSortStyle(sort)) {
+            return new SortCategory(sort);
+        }
+
+        return null;
+    }
 
     private ICategory tokenCategory(IToken token) {
         switch(token.getKind()) {
@@ -101,12 +120,10 @@ public class CategorizerService implements ICategorizerService<IStrategoTerm, IS
             case IToken.TK_ERROR_LAYOUT:
             case IToken.TK_LAYOUT:
                 return new TokenCategory("TK_LAYOUT");
-            case IToken.TK_ERROR:
-                return new TokenCategory("TK_ERROR");
-            case IToken.TK_UNKNOWN:
-                return new TokenCategory("TK_UNKNOWN");
             default:
                 logger.debug("Unhandled token kind " + token.getKind());
+            case IToken.TK_UNKNOWN:
+            case IToken.TK_ERROR:
             case IToken.TK_EOF:
             case IToken.TK_ERROR_EOF_UNEXPECTED:
             case IToken.TK_ESCAPE_OPERATOR:
--- org.metaborg.spoofax.core/src/main/java/org/metaborg/spoofax/core/style/RegionStyle.java
+++ org.metaborg.spoofax.core/src/main/java/org/metaborg/spoofax/core/style/RegionStyle.java
@@ -1,11 +1,13 @@
 package org.metaborg.spoofax.core.style;
 
+import javax.annotation.Nullable;
+
 import org.metaborg.spoofax.core.messages.ISourceRegion;
 
 public class RegionStyle<T> implements IRegionStyle<T> {
-    private final T fragment;
     private final ISourceRegion region;
     private final IStyle style;
+    @Nullable private final T fragment;
 
 
     public RegionStyle(T fragment, ISourceRegion region, IStyle style) {
@@ -15,9 +17,6 @@ public class RegionStyle<T> implements IRegionStyle<T> {
     }
 
 
-    @Override public T fragment() {
-        return fragment;
-    }
 
     @Override public ISourceRegion region() {
         return region;
@@ -25,5 +24,14 @@ public class RegionStyle<T> implements IRegionStyle<T> {
 
     @Override public IStyle style() {
         return style;
+    }
+
+    @Override public @Nullable T fragment() {
+        return fragment;
+    }
+
+
+    @Override public String toString() {
+        return String.format("RegionStyle [region=%s, style=%s, fragment=%s]", region, style, fragment);
     }
 }
