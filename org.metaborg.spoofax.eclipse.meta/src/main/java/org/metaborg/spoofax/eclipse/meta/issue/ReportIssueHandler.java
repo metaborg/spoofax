41480e660b07417e8898d665aa9d7a1dd33bda47
Gabriel Konat <gabrielkonat@gmail.com>
2015-03-27 14:45:08 +0100

    Implement report issue dialog.

--- org.metaborg.spoofax.eclipse.meta/src/main/java/org/metaborg/spoofax/eclipse/meta/issue/ReportIssueHandler.java
+++ org.metaborg.spoofax.eclipse.meta/src/main/java/org/metaborg/spoofax/eclipse/meta/issue/ReportIssueHandler.java
@@ -3,10 +3,22 @@ package org.metaborg.spoofax.eclipse.meta.issue;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.PlatformUI;
 
 public class ReportIssueHandler extends AbstractHandler {
     @Override public Object execute(ExecutionEvent event) throws ExecutionException {
-        // TODO Auto-generated method stub
+        Display.getDefault().asyncExec(new OpenDialogRunnable());
         return null;
+    }
+
+
+    private final class OpenDialogRunnable implements Runnable {
+        public void run() {
+            final Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
+            final ReportIssueDialog dialog = new ReportIssueDialog(shell);
+            dialog.open();
+        }
     }
 }
