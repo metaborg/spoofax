b501f29828121b5180304ea54ae37163781cd1a4
Gabriel Konat <gabrielkonat@gmail.com>
2016-01-12 11:13:53 +0100

    Require input directories in package builder, to rebuild when something is added or removed from the directory.

--- org.metaborg.spoofax.meta.core/src/main/java/org/metaborg/spoofax/meta/core/pluto/build/main/PackageBuilder.java
+++ org.metaborg.spoofax.meta.core/src/main/java/org/metaborg/spoofax/meta/core/pluto/build/main/PackageBuilder.java
@@ -22,6 +22,7 @@ import org.metaborg.spoofax.meta.core.pluto.build.PackSdf;
 import org.metaborg.spoofax.meta.core.pluto.build.Sdf2Rtg.Input;
 import org.metaborg.spoofax.meta.core.pluto.build.Sdf2Table;
 import org.metaborg.spoofax.meta.core.pluto.build.misc.Copy;
+import org.metaborg.spoofax.meta.core.pluto.stamp.DirectoryLastModifiedStamper;
 import org.metaborg.util.cmd.Arguments;
 
 import build.pluto.builder.BuildRequest;
@@ -126,6 +127,8 @@ public class PackageBuilder extends SpoofaxBuilder<SpoofaxInput, None> {
         final Collection<JarBuilder.Entry> fileEntries = Lists.newLinkedList();
 
         for(FileObject path : paths) {
+            final File pathFile = toFile(path);
+            require(pathFile, new DirectoryLastModifiedStamper());
             final FileObject[] files = path.findFiles(new AllFileSelector());
             if(files == null) {
                 continue;
