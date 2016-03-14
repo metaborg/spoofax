f973891fbe37be4b0f6c092b69ee42aee37d3d05
Eduardo Souza <eduardo7.ufv@gmail.com>
2016-03-14 15:40:00 +0100

    store offsets for the completions in the Completion

--- org.metaborg.core/src/main/java/org/metaborg/core/completion/Completion.java
+++ org.metaborg.core/src/main/java/org/metaborg/core/completion/Completion.java
@@ -2,23 +2,54 @@ package org.metaborg.core.completion;
 
 
 public class Completion implements ICompletion {
-    private final Iterable<ICompletionItem> items;
+    private Iterable<ICompletionItem> items;
     private final String description;
+    private final String text;
+    
+    private final int startOffset;
+    private final int endOffset;
+    
+    private boolean nested;
 
-
-    public Completion(Iterable<ICompletionItem> items, String description) {
-        this.items = items;
+    public Completion(String description, String text, int startOffset, int endOffset) {
         this.description = description;
+        this.text = text;
+        this.startOffset = startOffset;
+        this.endOffset = endOffset;
+        this.nested = false;
     }
-
 
     @Override public Iterable<ICompletionItem> items() {
         return items;
     }
 
-
     @Override public String toString() {
         return description;
     }
+    
+    @Override public String text(){
+        return text;
+    }
+    
+    public boolean isNested(){
+        return nested;
+    }
+    
+    public void setNested(boolean nested){
+        this.nested = nested;
+    }
+
+    @Override public int startOffset() {
+        return startOffset;
+    }
+
+    @Override public int endOffset() {
+        return endOffset;
+    }
+    
+    @Override public void setItems(Iterable<ICompletionItem> items){
+        this.items = items;
+    }
+    
     
 }
--- org.metaborg.core/src/main/java/org/metaborg/core/completion/ICompletion.java
+++ org.metaborg.core/src/main/java/org/metaborg/core/completion/ICompletion.java
@@ -2,4 +2,8 @@ package org.metaborg.core.completion;
 
 public interface ICompletion {
     Iterable<ICompletionItem> items();
+    String text();
+    int startOffset();
+    int endOffset();
+    void setItems(Iterable<ICompletionItem> items);
 }
