00b88e07a85d347d743a0926a5a8d0e5cf0e3dbe
Gabriel Konat <gabrielkonat@gmail.com>
2016-04-04 17:55:24 +0200

    Static injection required to inject language spec service.

--- org.metaborg.spoofax.meta.core/src/main/java/org/metaborg/spoofax/meta/core/SpoofaxExtensionModule.java
+++ org.metaborg.spoofax.meta.core/src/main/java/org/metaborg/spoofax/meta/core/SpoofaxExtensionModule.java
@@ -1,6 +1,7 @@
 package org.metaborg.spoofax.meta.core;
 
 import org.metaborg.spoofax.core.Spoofax;
+import org.metaborg.spoofax.core.SpoofaxModule;
 import org.metaborg.spoofax.meta.core.stratego.primitives.LanguageSpecNamePrimitive;
 import org.spoofax.interpreter.library.AbstractPrimitive;
 
@@ -10,11 +11,15 @@ import com.google.inject.multibindings.Multibinder;
 import com.google.inject.name.Names;
 
 /**
- * Module for extending {@link Spoofax}
+ * Module for extending {@link Spoofax}.
+ * 
+ * Note that this module is loaded together with a {@link SpoofaxModule}, so only those bindings are available. Bindings
+ * from {@link SpoofaxMetaModule} are NOT available. To inject bindings from {@link SpoofaxMetaModule}, use static
+ * injection and request injection in the {@link SpoofaxMetaModule}.
  */
 public class SpoofaxExtensionModule extends AbstractModule {
     @Override protected void configure() {
-        // Extend Spoofax primitive library.
+        // Extend Spoofax's primitive library.
         final Multibinder<AbstractPrimitive> spoofaxPrimitiveLibrary =
             Multibinder.newSetBinder(binder(), AbstractPrimitive.class, Names.named("SpoofaxPrimitiveLibrary"));
         spoofaxPrimitiveLibrary.addBinding().to(LanguageSpecNamePrimitive.class).in(Singleton.class);
--- org.metaborg.spoofax.meta.core/src/main/java/org/metaborg/spoofax/meta/core/stratego/primitives/LanguageSpecNamePrimitive.java
+++ org.metaborg.spoofax.meta.core/src/main/java/org/metaborg/spoofax/meta/core/stratego/primitives/LanguageSpecNamePrimitive.java
@@ -6,6 +6,8 @@ import org.metaborg.core.project.IProject;
 import org.metaborg.core.project.IProjectService;
 import org.metaborg.spoofax.meta.core.project.ISpoofaxLanguageSpec;
 import org.metaborg.spoofax.meta.core.project.ISpoofaxLanguageSpecService;
+import org.metaborg.util.log.ILogger;
+import org.metaborg.util.log.LoggerUtils;
 import org.spoofax.interpreter.core.IContext;
 import org.spoofax.interpreter.core.InterpreterException;
 import org.spoofax.interpreter.library.AbstractPrimitive;
@@ -13,17 +15,19 @@ import org.spoofax.interpreter.stratego.Strategy;
 import org.spoofax.interpreter.terms.IStrategoTerm;
 
 import com.google.inject.Inject;
+import com.google.inject.Provider;
 
 public class LanguageSpecNamePrimitive extends AbstractPrimitive {
-    private final IProjectService projectService;
-    private final ISpoofaxLanguageSpecService languageSpecService;
+    private static final ILogger logger = LoggerUtils.logger(LanguageSpecNamePrimitive.class);
 
-    @Inject public LanguageSpecNamePrimitive(IProjectService projectService,
-        ISpoofaxLanguageSpecService languageSpecService) {
+    @Inject private static Provider<ISpoofaxLanguageSpecService> languageSpecServiceProvider;
+
+    private final IProjectService projectService;
+
+    @Inject public LanguageSpecNamePrimitive(IProjectService projectService) {
         super("SSL_EXT_language_spec_name", 0, 0);
 
         this.projectService = projectService;
-        this.languageSpecService = languageSpecService;
     }
 
 
@@ -36,6 +40,13 @@ public class LanguageSpecNamePrimitive extends AbstractPrimitive {
             return false;
         }
 
+        if(languageSpecServiceProvider == null) {
+            // Indicates that meta-Spoofax is not available (ISpoofaxLanguageSpecService cannot be injected), but this
+            // should never happen because this primitive is inside meta-Spoofax. Check for null just in case.
+            logger.debug("Language specification service is not available; static injection failed");
+            return false;
+        }
+        final ISpoofaxLanguageSpecService languageSpecService = languageSpecServiceProvider.get();
         if(!languageSpecService.available(project)) {
             return false;
         }
@@ -43,7 +54,7 @@ public class LanguageSpecNamePrimitive extends AbstractPrimitive {
         try {
             languageSpec = languageSpecService.get(project);
         } catch(ConfigException e) {
-            throw new InterpreterException("Unable to get language specification project at " + location, e);
+            throw new InterpreterException("Unable to get language specification name for " + location, e);
         }
         if(languageSpec == null) {
             return false;
