b8134c7196317510a60ad94f58ba9d6f4c8c04af
Gabriel Konat <gabrielkonat@gmail.com>
2016-05-02 17:03:03 +0200

    Remove dependency on jgrapht library in Spoofax Core, implement topological sort manually.

--- org.metaborg.core/src/main/java/org/metaborg/core/build/BuildOrder.java
+++ org.metaborg.core/src/main/java/org/metaborg/core/build/BuildOrder.java
@@ -1,25 +1,27 @@
 package org.metaborg.core.build;
 
-import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
-import org.jgrapht.experimental.dag.DirectedAcyclicGraph;
 import org.metaborg.core.MetaborgRuntimeException;
 import org.metaborg.core.config.IGenerateConfig;
 import org.metaborg.core.config.ILanguageImplConfig;
 import org.metaborg.core.language.ILanguageImpl;
-import org.metaborg.util.iterators.Iterables2;
+import org.metaborg.util.collection.BiLinkedHashMultimap;
+import org.metaborg.util.collection.BiSetMultimap;
 
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
-
-import rx.functions.Func0;
+import com.google.common.collect.Sets;
 
 /**
  * Language build order calculation.
  */
 public class BuildOrder {
     private final Iterable<ILanguageImpl> languages;
-    private final DirectedAcyclicGraph<ILanguageImpl, Object> dag;
+    private final List<ILanguageImpl> buildOrder;
 
 
     /**
@@ -31,28 +33,57 @@ public class BuildOrder {
      *             When there is a cyclic dependency between languages.
      */
     public BuildOrder(Iterable<ILanguageImpl> languages) throws MetaborgRuntimeException {
+        final int size = Iterables.size(languages);
         this.languages = languages;
-        this.dag = new DirectedAcyclicGraph<>(Object.class);
+        this.buildOrder = Lists.newArrayListWithCapacity(size);
+        if(size == 0) {
+            return;
+        }
 
+        // We're only interested in dependencies between given languages, create a lookup map for them.
         final Map<String, ILanguageImpl> lookup = Maps.newHashMap();
         for(ILanguageImpl language : languages) {
-            dag.addVertex(language);
             lookup.put(language.belongsTo().name(), language);
         }
 
+        // Create dependency graph and set of languages that do not generate anything.
+        final Set<ILanguageImpl> noGenerates = Sets.newHashSet(languages);
+        final BiSetMultimap<ILanguageImpl, ILanguageImpl> generates = BiLinkedHashMultimap.create();
         for(ILanguageImpl source : languages) {
             final ILanguageImplConfig config = source.config();
+            boolean hasGenerates = false;
             for(IGenerateConfig generate : config.generates()) {
                 final ILanguageImpl target = lookup.get(generate.languageName());
                 if(target != null) {
-                    try {
-                        dag.addDagEdge(source, target);
-                    } catch(DirectedAcyclicGraph.CycleFoundException e) {
-                        throw new MetaborgRuntimeException(
-                            "Language implementations induce build cycle, cannot determine build order", e);
-                    }
+                    generates.put(source, target);
+                    hasGenerates = true;
                 }
             }
+            if(hasGenerates) {
+                noGenerates.remove(source);
+            }
+        }
+        if(noGenerates.isEmpty()) {
+            throw new MetaborgRuntimeException("Build order is cyclic");
+        }
+
+        // Execute Kahn's topological sort algorithm to create a build order and check for cycles.
+        while(!noGenerates.isEmpty()) {
+            final ILanguageImpl impl = Iterables.get(noGenerates, 0);
+            noGenerates.remove(impl);
+            buildOrder.add(impl);
+
+            // Copy collection to prevent ConcurrentModificationException by removal during iteration.
+            final Iterable<ILanguageImpl> allGeneratesFrom = Lists.newArrayList(generates.getInverse(impl));
+            for(ILanguageImpl generatesFrom : allGeneratesFrom) {
+                generates.remove(generatesFrom, impl);
+                if(!generates.containsKey(generatesFrom)) {
+                    noGenerates.add(generatesFrom);
+                }
+            }
+        }
+        if(!generates.isEmpty()) {
+            throw new MetaborgRuntimeException("Build order is cyclic");
         }
     }
 
@@ -60,11 +91,7 @@ public class BuildOrder {
      * @return Build order.
      */
     public Iterable<ILanguageImpl> buildOrder() {
-        return Iterables2.from(new Func0<Iterator<ILanguageImpl>>() {
-            @Override public Iterator<ILanguageImpl> call() {
-                return dag.iterator();
-            }
-        });
+        return buildOrder;
     }
 
     /**
