aa2811e8044032fb66e22361dbd424e58a43f5d7
Balletie <skip_meesie@hotmail.com>
2016-06-01 20:47:22 +0200

    Add default values for command prefix and evaluation method

--- org.metaborg.spoofax.core/src/main/java/org/metaborg/spoofax/core/shell/ShellFacetFromESV.java
+++ org.metaborg.spoofax.core/src/main/java/org/metaborg/spoofax/core/shell/ShellFacetFromESV.java
@@ -17,11 +17,13 @@ public class ShellFacetFromESV {
         return new ShellFacet(commandPrefix, evaluationMethod);
     }
 
-    private static @Nullable String commandPrefix(IStrategoAppl term) {
-        return ESVReader.getProperty(term, "CommandPrefix");
+    private static String commandPrefix(IStrategoAppl term) {
+        // Default is ":"
+        return ESVReader.getProperty(term, "CommandPrefix", ":");
     }
 
-    private static @Nullable String evaluationMethod(IStrategoAppl term) {
-        return ESVReader.getProperty(term, "EvaluationMethod");
+    private static String evaluationMethod(IStrategoAppl term) {
+        // Default is "dynsem"
+        return ESVReader.getProperty(term, "EvaluationMethod", "dynsem");
     }
 }
