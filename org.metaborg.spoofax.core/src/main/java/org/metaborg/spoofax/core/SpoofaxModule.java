91f1444bf2ebb458f9802e66d6d05a2f3b71ab33
Hendrik van Antwerpen <hendrik@van-antwerpen.net>
2017-02-03 19:14:29 +0100

    Remove obsolete sublist primitive.

--- org.metaborg.spoofax.core/src/main/java/org/metaborg/spoofax/core/SpoofaxModule.java
+++ org.metaborg.spoofax.core/src/main/java/org/metaborg/spoofax/core/SpoofaxModule.java
@@ -57,7 +57,6 @@ import org.metaborg.meta.nabl2.spoofax.primitives.SG_get_symbolic_facts;
 import org.metaborg.meta.nabl2.spoofax.primitives.SG_get_symbolic_goals;
 import org.metaborg.meta.nabl2.spoofax.primitives.SG_get_visible_decls;
 import org.metaborg.meta.nabl2.spoofax.primitives.SG_index_ast;
-import org.metaborg.meta.nabl2.spoofax.primitives.SG_index_sublist;
 import org.metaborg.meta.nabl2.spoofax.primitives.SG_set_ast_index;
 import org.metaborg.runtime.task.primitives.TaskLibrary;
 import org.metaborg.spoofax.core.action.ActionService;
@@ -367,7 +366,6 @@ public class SpoofaxModule extends MetaborgModule {
         bindPrimitive(spoofaxScopeGraphLibrary, SG_get_symbolic_goals.class);
         bindPrimitive(spoofaxScopeGraphLibrary, SG_get_visible_decls.class);
         bindPrimitive(spoofaxScopeGraphLibrary, SG_index_ast.class);
-        bindPrimitive(spoofaxScopeGraphLibrary, SG_index_sublist.class);
         bindPrimitive(spoofaxScopeGraphLibrary, SG_set_ast_index.class);
 
         final Multibinder<AbstractPrimitive> legacySpoofaxLibrary = Multibinder.newSetBinder(binder(),
