f20c14028eb50db0c8897ed35cf5ad3bbd8787e4
Hendrik van Antwerpen <hendrik@van-antwerpen.net>
2017-04-01 14:33:47 +0200

    Add configuration flag to control NaBL2 debug output.

--- org.metaborg.spoofax.core/src/main/java/org/metaborg/spoofax/core/analysis/constraint/AbstractConstraintAnalyzer.java
+++ org.metaborg.spoofax.core/src/main/java/org/metaborg/spoofax/core/analysis/constraint/AbstractConstraintAnalyzer.java
@@ -43,6 +43,7 @@ import org.metaborg.spoofax.core.tracing.ISpoofaxTracingService;
 import org.metaborg.spoofax.core.unit.ISpoofaxParseUnit;
 import org.metaborg.util.iterators.Iterables2;
 import org.metaborg.util.log.ILogger;
+import org.metaborg.util.log.Level;
 import org.metaborg.util.log.LoggerUtils;
 import org.metaborg.util.resource.ResourceUtils;
 import org.metaborg.util.task.ICancel;
@@ -238,6 +239,10 @@ abstract class AbstractConstraintAnalyzer<C extends ISpoofaxScopeGraphContext<?>
             }
             return text;
         };
+    }
+
+    protected Level debugLevel(C context) {
+        return context.debug() ? Level.Info : Level.Debug;
     }
 
 }
