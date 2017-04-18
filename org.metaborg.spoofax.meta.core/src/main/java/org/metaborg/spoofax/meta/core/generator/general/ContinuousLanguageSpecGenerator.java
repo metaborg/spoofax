4506d7597bc7151f693a7546e487b49d79d8beea
Gabriel Konat <gabrielkonat@gmail.com>
2017-04-18 13:46:15 +0200

    Fix completions.str being generated when SDF is disabled.

--- org.metaborg.spoofax.meta.core/src/main/java/org/metaborg/spoofax/meta/core/generator/general/ContinuousLanguageSpecGenerator.java
+++ org.metaborg.spoofax.meta.core/src/main/java/org/metaborg/spoofax/meta/core/generator/general/ContinuousLanguageSpecGenerator.java
@@ -2,6 +2,8 @@ package org.metaborg.spoofax.meta.core.generator.general;
 
 import java.io.IOException;
 
+import javax.annotation.Nullable;
+
 import org.metaborg.spoofax.meta.core.config.SdfVersion;
 import org.metaborg.spoofax.meta.core.generator.BaseGenerator;
 import org.metaborg.spoofax.meta.core.generator.GeneratorSettings;
@@ -11,22 +13,26 @@ import org.metaborg.util.file.IFileAccess;
  * Generates project files which need to be generated after each build. Files are not specific to an IDE.
  */
 public class ContinuousLanguageSpecGenerator extends BaseGenerator {
-    private final SdfVersion version;
+    private final boolean sdfEnabled;
+    private final @Nullable SdfVersion version;
 
 
-    public ContinuousLanguageSpecGenerator(GeneratorSettings scope, IFileAccess access, SdfVersion version) {
+    public ContinuousLanguageSpecGenerator(GeneratorSettings scope, IFileAccess access, boolean sdfEnabled,
+        @Nullable SdfVersion version) {
         super(scope, access);
+        this.sdfEnabled = sdfEnabled;
         this.version = version;
     }
 
-    public ContinuousLanguageSpecGenerator(GeneratorSettings scope, SdfVersion version) {
+    public ContinuousLanguageSpecGenerator(GeneratorSettings scope, boolean sdfEnabled, @Nullable SdfVersion version) {
         super(scope);
+        this.sdfEnabled = sdfEnabled;
         this.version = version;
     }
 
 
     public void generateAll() throws IOException {
-        if(version == SdfVersion.sdf3) {
+        if(sdfEnabled && version == SdfVersion.sdf3) {
             generateCompletionStrategies();
         }
     }
