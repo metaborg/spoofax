a5413402daf5c573516ecd78386c29d07fbac3da
Hendrik van Antwerpen <hendrik@van-antwerpen.net>
2018-05-07 16:20:40 +0200

    Rename primitive.

--- org.metaborg.spoofax.core/src/main/java/org/metaborg/spoofax/core/SpoofaxModule.java
+++ org.metaborg.spoofax.core/src/main/java/org/metaborg/spoofax/core/SpoofaxModule.java
@@ -207,7 +207,7 @@ import mb.nabl2.spoofax.primitives.SG_is_debug_collection_enabled;
 import mb.nabl2.spoofax.primitives.SG_is_debug_custom_enabled;
 import mb.nabl2.spoofax.primitives.SG_is_debug_resolution_enabled;
 import mb.nabl2.spoofax.primitives.SG_set_ast_index;
-import mb.nabl2.spoofax.primitives.SG_solve;
+import mb.nabl2.spoofax.primitives.SG_solve_constraint;
 
 /**
  * Guice module that specifies which implementations to use for services and factories.
@@ -429,7 +429,7 @@ public class SpoofaxModule extends MetaborgModule {
         bindPrimitive(spoofaxScopeGraphLibrary, SG_is_debug_custom_enabled.class);
         bindPrimitive(spoofaxScopeGraphLibrary, SG_is_debug_resolution_enabled.class);
         bindPrimitive(spoofaxScopeGraphLibrary, SG_set_ast_index.class);
-        bindPrimitive(spoofaxScopeGraphLibrary, SG_solve.class);
+        bindPrimitive(spoofaxScopeGraphLibrary, SG_solve_constraint.class);
 
         final Multibinder<AbstractPrimitive> spoofaxFlowSpecLibrary =
             Multibinder.newSetBinder(binder(), AbstractPrimitive.class, Names.named(FlowSpecLibrary.name));
