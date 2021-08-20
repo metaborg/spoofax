67710f191ae7ba70c0d1f6a386ca2afc11577eb1
Aron Zwaan <aronzwaan@gmail.com>
2021-08-20 10:07:20 +0200

    Read/Write suppress cascading errors config option.

--- org.metaborg.spoofax.core/src/main/java/org/metaborg/spoofax/core/config/language/StatixProjectConfigReaderWriter.java
+++ org.metaborg.spoofax.core/src/main/java/org/metaborg/spoofax/core/config/language/StatixProjectConfigReaderWriter.java
@@ -23,12 +23,13 @@ public class StatixProjectConfigReaderWriter {
     private static final String PROP_MESSAGE_TRACE_LENGTH = "message-trace-length";
     private static final String PROP_MESSAGE_TERM_DEPTH = "message-term-depth";
     private static final String PROP_TESTLOG = "test-log";
+    private static final String PROP_SUPPRESS_CASCADING_ERRORS = "suppress-cascading-errors";
 
     public static IStatixProjectConfig read(HierarchicalConfiguration<ImmutableNode> config) {
         Collection<String> parallelLanguages = config.getList(String.class, PROP_CONCURRENT, Collections.emptyList());
         Map<String, SolverMode> modes = new HashMap<>();
         if(!parallelLanguages.isEmpty()) {
-            logger.warn("Option runtime.statix.concurrent is deprecated. Use runtime.statix.modes.<lang>.<mode> instead.");
+            logger.warn("Option runtime.statix.concurrent is deprecated. Use runtime.statix.modes.<lang>: <mode> instead.");
         }
         parallelLanguages.forEach(languageName -> modes.put(languageName, SolverMode.CONCURRENT));
         config.getKeys(PROP_MODES).forEachRemaining(modeKey -> {
@@ -39,7 +40,8 @@ public class StatixProjectConfigReaderWriter {
         Integer messageStacktraceLength = config.getInteger(PROP_MESSAGE_TRACE_LENGTH, null);
         Integer messageTermDepth = config.getInteger(PROP_MESSAGE_TERM_DEPTH, null);
         String testLog = config.getString(PROP_TESTLOG, null);
-        return new StatixProjectConfig(modes, messageStacktraceLength, messageTermDepth, testLog);
+        Boolean suppressCascadingErrors = config.getBoolean(PROP_SUPPRESS_CASCADING_ERRORS, true);
+        return new StatixProjectConfig(modes, messageStacktraceLength, messageTermDepth, testLog, suppressCascadingErrors);
     }
 
     public static void write(IStatixProjectConfig statixConfig, HierarchicalConfiguration<ImmutableNode> config) {
@@ -56,6 +58,14 @@ public class StatixProjectConfigReaderWriter {
         final Integer messageTermDepth = statixConfig.messageTermDepth(null);
         if(messageTermDepth != null) {
             config.setProperty(PROP_MESSAGE_TERM_DEPTH, messageTermDepth);
+        }
+        final String testLog = statixConfig.testLogLevel(null);
+        if(testLog != null) {
+            config.setProperty(PROP_TESTLOG, testLog);
+        }
+        final Boolean suppressCascadingErrors = config.getBoolean(PROP_SUPPRESS_CASCADING_ERRORS, null);
+        if(suppressCascadingErrors != null) {
+            config.setProperty(PROP_SUPPRESS_CASCADING_ERRORS, suppressCascadingErrors);
         }
     }
 
