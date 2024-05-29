f41efdf527ea20e5627c7347d70e4504a5b45da6
Jeff Smits <j.smits-1@tudelft.nl>
2024-05-29 14:06:04 +0200

    Fix missing compositeBuild fn in gradle, add explicit dep on o.m.util in o.m.meta.core

--- org.metaborg.meta.core/build.gradle.kts
+++ org.metaborg.meta.core/build.gradle.kts
@@ -2,11 +2,13 @@ plugins {
     id("org.metaborg.gradle.config.java-library")
 }
 
+fun compositeBuild(name: String) = "$group:$name:$version"
 val spoofax2Version: String by ext
 dependencies {
     api(platform("org.metaborg:parent:$spoofax2Version"))
 
     api(project(":org.metaborg.core"))
+    api(compositeBuild("org.metaborg.util"))
     api(compositeBuild("util-vfs2"))
 
     implementation("jakarta.annotation:jakarta.annotation-api")
