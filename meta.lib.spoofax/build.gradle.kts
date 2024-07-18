7bc714130017ee6f028bf7bb53dfe34f363b7799
Daniel A. A. Pelsmaeker <developer@pelsmaeker.net>
2024-07-18 16:16:44 +0200

    Specify spoofax.core and repo manually

--- meta.lib.spoofax/build.gradle.kts
+++ meta.lib.spoofax/build.gradle.kts
@@ -1,4 +1,18 @@
 plugins {
-    id("org.metaborg.devenv.spoofax.gradle.langspec")
+    `java-library`
     `maven-publish`
+    id("org.metaborg.convention.java")
+    id("org.metaborg.convention.maven-publish")
+    id("org.metaborg.devenv.spoofax.gradle.langspec")
+}
+
+spoofaxLanguageSpecification {
+    // We add the dependency manually and don't change the repositories
+    // Eventually, this functionality should be removed from spoofax.gradle
+    addSpoofaxCoreDependency.set(false)
+    addSpoofaxRepository.set(false)
+}
+
+dependencies {
+    compileOnly(libs.spoofax2.core)
 }
